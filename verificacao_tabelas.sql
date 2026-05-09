4c47e324db409eedab566cc63c1862a885fe5729
Ana Giulia Aguiar <166962330+imanagiulia@users.noreply.github.com>
2026-05-08 23:06:52 -0300

    Verificação/visualização dos dados nas tabelas

---
+++ verificacao_tabelas.sql
@@ -0,0 +1,13 @@
+SELECT * from alunos;
+
+SELECT * from professores;
+
+SELECT * from disciplinas;
+
+SELECT * from notas;
+
+SELECT * from turmas;
+
+SELECT * from turma_alunos;
+
+SELECT * from turmas_disciplinas;
